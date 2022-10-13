a8b838773594c2584a29ae181fe5ce9e0cb40cd6
butthead <simonpatrikoff1@outlook.com>
2022-10-13 14:43:55 +1000

    suck

---
+++ hight/build/hight/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/butthead/WS/robrep/hight/nuc/hight
+# Build directory: /home/butthead/WS/robrep/hight/nuc/hight/build/hight
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
